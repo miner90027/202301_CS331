b13ea4686cfa099eec99aa1142c47635a1c5166d
miner90027 <alekszander.mccormick@outlook.com>
2023-02-13 17:19:42 -0900

    Wrote mapTable and concatMax functions

--- Assignment_2/pa2.lua
+++ Assignment_2/pa2.lua
@@ -23,11 +23,19 @@ local pa2 = {}
 --[[***********************************]]--
 
 -- mapTable
--- Takes in one-paraperter function and a table. Returns a new table of key-value pairs
+-- Takes in a one-paraperter function and a table. Returns a new table of key-value pairs
 --   	for each key-value pair in the provided table
 -- Exported
-function pa2.mapTable(temp)
-	return nil
+function pa2.mapTable(func, tbl)
+    
+    local output = {}
+        
+    for key, val in pairs(tbl) do
+       output[key] = func(val)
+       -- table.insert(output, [key]=func(val))
+    end
+    
+	return output
 end
 
 
@@ -35,8 +43,15 @@ end
 -- Takes a string and integer. Returns a sring concatanation of as many copies of the given
 --   	string as possible without exceeding a lenght greater than the given integer.
 -- Exported
-function pa2.concatMax(temp)
-	retrun nil
+function pa2.concatMax(str, max)
+
+    local output = ""
+
+    while(string.len(output) + string.len(str) <= max) do
+        output = output..str
+    end
+    
+	return output
 end
 
 -- collatz
