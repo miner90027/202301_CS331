061d6730c9105406dbbb47469a5523d02e077958
miner90027 <alekszander.mccormick@outlook.com>
2023-02-15 14:40:51 -0900

    Initial creation of lexit.lua Ran check_forth.fs and took note of the secret message.

---
+++ Assignment_3/lexit.lua
@@ -0,0 +1,21 @@
+#!/usr/bin/env lua
+
+--[[
+  - lexit.lua
+  - Aleks McCormick
+  - 2023/02/15
+  - Spring 2023 CS 331
+  - Assignment 3 Lexer for instrucor created Maleo language
+--]]
+
+--[[***********************************]]--
+--[[***          Section A          ***]]--
+--[[***********************************]]--
+
+	-- Secret Message: I am one with the Forth. The Forth is with me.
+
+--[[***********************************]]--
+--[[***          Section B          ***]]--
+--[[***********************************]]--
+
+
