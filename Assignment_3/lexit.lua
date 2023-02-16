46b902474503282bdf8a6a760166783912364635
miner90027 <alekszander.mccormick@outlook.com>
2023-02-15 15:06:42 -0900

    Created numeric constants that represent the lexeme categories.

--- Assignment_3/lexit.lua
+++ Assignment_3/lexit.lua
@@ -5,17 +5,50 @@
   - Aleks McCormick
   - 2023/02/15
   - Spring 2023 CS 331
-  - Assignment 3 Lexer for instrucor created Maleo language
+  - Assignment 3
+  -     Lexer for instrucor created Maleo language
 --]]
 
---[[***********************************]]--
 --[[***          Section A          ***]]--
---[[***********************************]]--
 
 	-- Secret Message: I am one with the Forth. The Forth is with me.
 
---[[***********************************]]--
 --[[***          Section B          ***]]--
+
+
+-- Initilize module table
+local lexit = {}
+
+--[[***********************************]]--
+--[[***      Public Constants       ***]]--
 --[[***********************************]]--
 
+-- Numeric constants representing the different lexeme categories
+lexit.KEY    = 1
+lexit.ID     = 2
+lexit.NUMLIT = 3
+lexit.STRLIT = 4
+lexit.OP     = 5
+lexit.PUNCT  = 6
+lexit.MAL    = 7
 
+-- catnames
+-- Array of human-readable lexeme categories in as strings. The 
+--      indacies of the categories are the above numeric constants.
+lexer.catnames = {
+    "Keyword",
+    "Identifier",
+    "NumericLiteral",
+    "StringLiteral",
+    "Operator",
+    "Punctuation",
+    "Malformed"
+}
+
+
+
+
+
+
+-- return module table
+return lexit
