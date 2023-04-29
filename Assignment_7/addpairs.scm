02de20608196b985b44b4304e7ec9d611c5b554c
miner90027 <alekszander.mccormick@outlook.com>
2023-04-29 14:47:27 -0800

    Finished writing the addpairs scheme procedure

--- Assignment_7/addpairs.scm
+++ Assignment_7/addpairs.scm
@@ -1,3 +1,18 @@
 #lang scheme
 
+#|
+ | addpairs.scm
+ | 2023/04/29
+ | Spring 2023 CS 331
+ | Assignment 7 Part 3
+ |      A scheme procedure that adds pairs
+ |          of numbers.
+ |#
 
+(define (addpairs . args)
+    (cond
+        ((null? args) args)
+        ((null? (cdr args)) args)
+        (else (cons(+ (car args) (cadr args)) (apply addpairs (cddr args))))
+    )
+)
