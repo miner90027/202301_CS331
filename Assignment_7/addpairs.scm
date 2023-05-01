d91e640b5d02a392abe1bddfa3393d4ee3b2565c
Aleks M <42454990+miner90027@users.noreply.github.com>
2023-05-01 13:56:37 -0800

    Update addpairs.scm
    Added more detailed comments

--- Assignment_7/addpairs.scm
+++ Assignment_7/addpairs.scm
@@ -6,13 +6,17 @@
  | Spring 2023 CS 331
  | Assignment 7 Part 3
  |      A scheme procedure that adds pairs
- |          of numbers.
+ |          of numbers
  |#
 
 (define (addpairs . args)
     (cond
+        ; Check if current value is null
         ((null? args) args)
+        ; Check if next value is null
         ((null? (cdr args)) args)
+        ; Add the current value and the next value
+        ;   then recurse over the rest of the values
         (else (cons(+ (car args) (cadr args)) (apply addpairs (cddr args))))
     )
 )
