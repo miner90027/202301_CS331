8e2daa8c9039084d7b3de3fbd4e79aa25f078608
Aleks M <42454990+miner90027@users.noreply.github.com>
2023-05-01 14:02:26 -0800

    Update collcount.fs
    Updated comments

--- Assignment_7/collcount.fs
+++ Assignment_7/collcount.fs
@@ -3,10 +3,10 @@
   - Aleks McCormick
   - 2023/04/28
   - Spring 2023 CS 331
-  - Assignment 7 Exersize 2
-  -   Takes in a number and returns the 
-  -      number of itterations of the Collatz
-- )
+  - Assignment 7 Exercise 2
+  -   The same thing as the collatzCounts function from
+  -      Assignment 5, but this time writen in Forth
+  - )
 
 \ collcount
 \ Takes a positive integer and returns the number of iterations
@@ -15,14 +15,15 @@
 : collcount { n -- c }
     0
     begin
+    \ loop based collatz function
     n 1 > while
         n 2 mod 0 = if
             n 2 / to n
         else
             3 n * 1 + to n 
-        endif    
+        endif
+        \ Increment counter variable c
         1 +
-    
     repeat
 ;    
 
