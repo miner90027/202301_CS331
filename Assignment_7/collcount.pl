bdf888626141290d9e0976e4522d0d4b888e1cf7
Aleks M <42454990+miner90027@users.noreply.github.com>
2023-05-01 14:14:34 -0800

    Update collcount.pl
    Updated comments

--- Assignment_7/collcount.pl
+++ Assignment_7/collcount.pl
@@ -3,26 +3,29 @@
  * Aleks McCormick
  * 2023/04/29
  * Spring 2023 CS 331
- * Assignment 7 Part 4
- * 
+ * Assignment 7 Exercise 4
+ *   The same thing as Assignment 7 Exercise 2
+ *      but written in Prolog
  */
 
-%  
+% collcount\2
+% collcount(+n, ?c)
+
+%  Case when N is 1
 collcount(1, 0). 
 
 % Case when n is even
-%   when n is even c is n/2
 collcount(N, C) :-
-    N > 1,
-    N mod 2 =:= 0,
-    Nr is N / 2,
-    collcount(Nr, Cr),
-    C is Cr + 1.
+    N > 1,              % Verify N > 1
+    N mod 2 =:= 0,      % Verify N is even
+    Nr is N / 2,        % Set Nr to N/2
+    collcount(Nr, Cr),  % Recurse passing Nr and receiving Cr
+    C is Cr + 1.        % Set C equal to Cr + 1 and return C
 
 % Case when n is odd
 collcount(N, C) :-
-    N > 1,
-    N mod 2 =\= 0,
-    Nr is (3 * N + 1),
-    collcount(Nr, Cr),
-    C is Cr + 1.
+    N > 1,              % Verify N > 1
+    N mod 2 =\= 0,      % Verify N is odd
+    Nr is (3 * N + 1),  % Set Nr to 3N+1
+    collcount(Nr, Cr),  % Recurse passing Nr and receiving Cr
+    C is Cr + 1.        % Set C equal to Cr + 1 and return C
